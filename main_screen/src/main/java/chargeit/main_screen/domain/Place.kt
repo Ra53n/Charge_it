57b8cb33986229500aee979fc96bf9f66dc33abc
geekbrains <info@geekbrains.ru>
2022-11-05 17:25:51 +0300

    Проведён рефакторинг кода

--- main_screen/src/main/java/chargeit/main_screen/domain/Place.kt
+++ main_screen/src/main/java/chargeit/main_screen/domain/Place.kt
@@ -1,10 +1,11 @@
 package chargeit.main_screen.domain
 
+import chargeit.main_screen.settings.*
 import com.google.android.gms.maps.model.LatLng
 
 data class Place(
-    val name: String = "",
-    val tag: String = "",
-    val coordinates: LatLng = LatLng(0.0, 0.0),
-    val zoomLevel: Float = 9.0f,
+    val name: String = DEFAULT_PLACE_NAME,
+    val tag: String = DEFAULT_PLACE_TAG,
+    val coordinates: LatLng = LatLng(DEFAULT_PLACE_LATITUDE, DEFAULT_PLACE_LONGITUDE),
+    val zoomLevel: Float = DEFAULT_PLACE_INIT_ZOOM_LEVEL,
 )
