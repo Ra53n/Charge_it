7e5348b7b9198c7f9339cd1a130fd76397586e9b
geekbrains <info@geekbrains.ru>
2022-11-05 16:11:40 +0300

    Реализован поиск адресов по карте

--- main_screen/src/main/java/chargeit/main_screen/domain/Place.kt
+++ main_screen/src/main/java/chargeit/main_screen/domain/Place.kt
@@ -3,8 +3,8 @@ package chargeit.main_screen.domain
 import com.google.android.gms.maps.model.LatLng
 
 data class Place(
-    val name: String,
-    val tag: String,
-    val coordinates: LatLng,
-    val zoomLevel: Float,
+    val name: String = "",
+    val tag: String = "",
+    val coordinates: LatLng = LatLng(0.0, 0.0),
+    val zoomLevel: Float = 9.0f,
 )
--- main_screen/src/main/java/chargeit/main_screen/view/MapsFragment.kt
+++ main_screen/src/main/java/chargeit/main_screen/view/MapsFragment.kt
@@ -1,5 +1,6 @@
 package chargeit.main_screen.view
 
+import android.location.Geocoder
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,16 +10,21 @@ import chargeit.core.view.CoreFragment
 import chargeit.main_screen.R
 import chargeit.main_screen.databinding.FragmentMapsBinding
 import chargeit.main_screen.domain.Place
+import chargeit.main_screen.settings.ADDRESS_SEARCH_ZOOM_LEVEL
+import chargeit.main_screen.settings.MAX_ADDRESS_SEARCH_RESULTS
+import chargeit.main_screen.settings.ZERO_INT
 import chargeit.main_screen.utils.hideKeyboard
 import chargeit.main_screen.utils.makeSnackbar
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import java.lang.String.format
 
 class MapsFragment : CoreFragment(R.layout.fragment_maps) {
 
@@ -26,6 +32,8 @@ class MapsFragment : CoreFragment(R.layout.fragment_maps) {
     private val binding get() = _binding!!
     private lateinit var map: GoogleMap
     private val mainScreenViewModel: MapsFragmentViewModel by viewModel()
+    private var currentAddressMarker: Marker? = null
+    private var currentUserMarker: Marker? = null
 
     private val callback = OnMapReadyCallback { googleMap ->
         map = googleMap
@@ -53,9 +61,16 @@ class MapsFragment : CoreFragment(R.layout.fragment_maps) {
         binding.fragmentMapsSearch.setOnQueryTextListener(
             object : SearchView.OnQueryTextListener {
                 override fun onQueryTextSubmit(query: String?): Boolean {
-                    view?.let {
-                        it.hideKeyboard()
-                        it.makeSnackbar(text = START_SEARCH_MESSAGE)
+                    view?.let { thisView ->
+                        query?.let { thisQuery ->
+                            thisView.hideKeyboard()
+                            val resQuery = thisQuery.lowercase().trim()
+                            if (resQuery.isNotBlank()) {
+                                searchByAddress(resQuery, thisView)
+                            } else {
+                                thisView.makeSnackbar(text = EMPTY_QUERY_MESSAGE)
+                            }
+                        }
                     }
                     return true
                 }
@@ -75,11 +90,9 @@ class MapsFragment : CoreFragment(R.layout.fragment_maps) {
     }
 
     private fun initMap() {
-        val defaultPlace = mainScreenViewModel.getDefaultPlace()
-        goToPlace(defaultPlace)
-        map.addMarker(MarkerOptions().position(defaultPlace.coordinates).title(defaultPlace.name))
+        goToDefaultPlace()
         map.setOnMarkerClickListener { marker ->
-            view?.makeSnackbar(text = format(MARKER_CLICK_MESSAGE, marker.title))
+            view?.makeSnackbar(text = marker.title ?: NO_TITLE_MESSAGE)
             true
         }
     }
@@ -91,7 +104,11 @@ class MapsFragment : CoreFragment(R.layout.fragment_maps) {
     }
 
     private fun goToDefaultPlace() {
-        goToPlace(mainScreenViewModel.getDefaultPlace())
+        val defaultPlace = mainScreenViewModel.getDefaultPlace()
+        currentUserMarker?.remove()
+        currentUserMarker =
+            addMarker(defaultPlace.coordinates, defaultPlace.name, R.drawable.ic_map_marker)
+        goToPlace(defaultPlace)
     }
 
     private fun goToPlace(place: Place) {
@@ -103,6 +120,44 @@ class MapsFragment : CoreFragment(R.layout.fragment_maps) {
         view?.makeSnackbar(text = DEFAULT_PLACE_MESSAGE)
     }
 
+    private fun searchByAddress(query: String, view: View) {
+        Thread {
+            try {
+                val geoCoder = Geocoder(view.context)
+                val addresses = geoCoder.getFromLocationName(query, MAX_ADDRESS_SEARCH_RESULTS)
+                if (addresses != null && addresses.size > ZERO_INT) {
+                    val address = addresses[ZERO_INT]
+                    view.post {
+                        goToPlace(
+                            Place(
+                                name = address.getAddressLine(ZERO_INT),
+                                coordinates = LatLng(address.latitude, address.longitude),
+                                zoomLevel = ADDRESS_SEARCH_ZOOM_LEVEL
+                            )
+                        )
+                        currentAddressMarker?.remove()
+                        currentAddressMarker = map.addMarker(
+                            MarkerOptions().position(LatLng(address.latitude, address.longitude))
+                                .title(address.getAddressLine(ZERO_INT))
+                        )
+                    }
+                } else {
+                    view.makeSnackbar(text = ADDRESS_IS_NOT_FOUND_MESSAGE)
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }.start()
+    }
+
+    private fun addMarker(position: LatLng, title: String, icon: Int? = null): Marker? {
+        val options = MarkerOptions().position(position).title(title)
+        icon?.let {
+            options.icon(BitmapDescriptorFactory.fromResource(icon))
+        }
+        return map.addMarker(options)
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
@@ -110,8 +165,9 @@ class MapsFragment : CoreFragment(R.layout.fragment_maps) {
 
     companion object {
         private const val DEFAULT_PLACE_MESSAGE = "Default place"
-        private const val MARKER_CLICK_MESSAGE = "Marker %1\$s clicked!"
-        private const val START_SEARCH_MESSAGE = "Searching!"
+        private const val EMPTY_QUERY_MESSAGE = "Empty query!"
+        private const val NO_TITLE_MESSAGE = "No title!"
+        private const val ADDRESS_IS_NOT_FOUND_MESSAGE = "Address is not found!"
 
         @JvmStatic
         fun newInstance() = MapsFragment()
