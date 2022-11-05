8d6d5bc1d69cf0cbcc7001fe70e560f68eaaedc4
geekbrains <info@geekbrains.ru>
2022-11-05 16:11:40 +0300

    Реализован поиск адресов по карте

--- main_screen/src/main/java/chargeit/main_screen/settings/Settings.kt
+++ main_screen/src/main/java/chargeit/main_screen/settings/Settings.kt
@@ -1,5 +1,9 @@
 package chargeit.main_screen.settings
 
+const val ZERO_INT = 0
+const val ADDRESS_SEARCH_ZOOM_LEVEL = 10.0F
+const val MAX_ADDRESS_SEARCH_RESULTS = 5
+
 // Default place information
 const val DEFAULT_PLACE_NAME = "Moscow"
 const val DEFAULT_PLACE_TAG = "MOSCOW"
