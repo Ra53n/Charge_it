79242af0a5fb998190cf82cac354bfe46de6c17f
geekbrains <info@geekbrains.ru>
2022-11-08 05:34:40 +0300

    1. Добавлены свойства-расширения для некоторых типов данных 2. Реализовано(в черновом виде) определение текущих координат пользователя

--- main_screen/src/main/java/chargeit/main_screen/settings/Settings.kt
+++ main_screen/src/main/java/chargeit/main_screen/settings/Settings.kt
@@ -1,18 +1,17 @@
 package chargeit.main_screen.settings
 
-// Primary constants
-const val ZERO_INT = 0
-const val ZERO_FLOAT = 0.0F
-const val ZERO_DOUBLE = 0.0
-const val EMPTY_STRING = ""
-
 // Address search information
-const val ADDRESS_SEARCH_ZOOM_LEVEL = 10.0F
+const val ADDRESS_SEARCH_ZOOM_LEVEL = 10.0f
 const val MAX_ADDRESS_SEARCH_RESULTS = 5
 
+// User location information
+const val USER_LOCATION_REFRESH_PERIOD = 60000L
+const val USER_LOCATION_MINIMAL_DISTANCE = 100f
+const val USER_LOCATION_ZOOM_LEVEL = 15f
+const val USER_LOCATION_SEARCH_RESULTS = 1
+
 // Default place information
-const val DEFAULT_PLACE_NAME = "Moscow"
-const val DEFAULT_PLACE_TAG = "MOSCOW"
 const val DEFAULT_PLACE_LATITUDE = 55.751513
 const val DEFAULT_PLACE_LONGITUDE = 37.616655
-const val DEFAULT_PLACE_INIT_ZOOM_LEVEL = 9.0F
+const val DEFAULT_PLACE_ZOOM_LEVEL = 10.0f
+
--- main_screen/src/main/java/chargeit/main_screen/view/MapsFragmentViewModel.kt
+++ main_screen/src/main/java/chargeit/main_screen/view/MapsFragmentViewModel.kt
@@ -1,8 +1,7 @@
 package chargeit.main_screen.view
 
 import chargeit.core.viewmodel.CoreViewModel
-import chargeit.main_screen.domain.Place
 
 class MapsFragmentViewModel : CoreViewModel() {
-    fun getDefaultPlace() = Place()
+
 }
