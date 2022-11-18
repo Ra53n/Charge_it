e03ce9d904bc05fd96669aa3bfffefab11354b67
geekbrains <info@geekbrains.ru>
2022-11-18 13:32:57 +0300

    1. ViewModel теперь передаёт данные во View только через LiveData 2. Маркеры создаются во ViewModel 3. Механизм отслеживания текущих координат устройства заменён на более современный 4. Проведён общий рефакторинг кода (в том числе переименования)

--- main_screen/src/main/java/chargeit/main_screen/di/modules.kt
+++ main_screen/src/main/java/chargeit/main_screen/di/modules.kt
@@ -1,24 +1,11 @@
 package chargeit.main_screen.di
 
-import androidx.lifecycle.MutableLiveData
-import chargeit.main_screen.domain.DatasetState
-import chargeit.main_screen.domain.LocationState
 import chargeit.main_screen.ui.MapsFragmentViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
-import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val mainScreenModule = module {
 
-    single<MutableLiveData<DatasetState>>(named("DatasetState")) { MutableLiveData() }
-    single<MutableLiveData<LocationState>>(named("LocationState")) { MutableLiveData() }
-
-    viewModel {
-        MapsFragmentViewModel(
-            get(),
-            get(named("LocationState")),
-            get(named("DatasetState"))
-        )
-    }
+    viewModel { MapsFragmentViewModel(get()) }
 
 }
