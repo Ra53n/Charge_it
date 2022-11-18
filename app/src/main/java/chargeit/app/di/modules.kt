03bfbde7e66536ff097096dcabacd451378c6b56
Oduvan58 <yancheuskipavel@gmail.com>
2022-11-18 20:52:44 +0300

    Fixed comments and starting new task

--- app/src/main/java/chargeit/app/di/modules.kt
+++ app/src/main/java/chargeit/app/di/modules.kt
@@ -1,6 +1,11 @@
 package chargeit.app.di
 
 import chargeit.data.repository.*
+import chargeit.data.room.mappers.CarModelToEntityMapper
+import chargeit.data.room.mappers.ElectricStationModelToEntityMapper
+import chargeit.data.room.mappers.SocketModelToEntityMapper
+import chargeit.data.room.mappers.UserModelToEntityMapper
+import org.koin.core.scope.get
 import org.koin.dsl.module
 
 val appModule = module {
@@ -9,5 +14,10 @@ val appModule = module {
     single<LocalElectricStationRepo> { LocalElectricStationRepoImpl(get()) }
     single<LocalSocketRepo> { LocalSocketRepoImpl(get()) }
     single<LocalUserRepo> { LocalUserRepoImpl(get()) }
+
+    single { CarModelToEntityMapper(get()) }
+    single { ElectricStationModelToEntityMapper(get()) }
+    single { SocketModelToEntityMapper() }
+    single { UserModelToEntityMapper(get()) }
 
 }
