9616a33854f5608a7d22b3337b4d63831728fb98
Stellarworker <75096455+Stellarworker@users.noreply.github.com>
2022-11-20 10:45:44 +0300

    Feature/main screen
    Создан основной экран приложения

--- app/src/main/java/chargeit/app/di/modules.kt
+++ app/src/main/java/chargeit/app/di/modules.kt
@@ -1,20 +1,5 @@
 package chargeit.app.di
 
-import chargeit.data.repository.*
-import chargeit.data.room.mappers.CarModelToEntityMapper
-import chargeit.data.room.mappers.ElectricStationModelToEntityMapper
-import chargeit.data.room.mappers.UserModelToEntityMapper
 import org.koin.dsl.module
 
-val appModule = module {
-
-    single<LocalCarRepo> { LocalCarRepoImpl(get()) }
-    single<LocalElectricStationRepo> { LocalElectricStationRepoImpl(get()) }
-    single<LocalSocketRepo> { LocalSocketRepoImpl() }
-    single<LocalUserRepo> { LocalUserRepoImpl(get()) }
-
-    single { CarModelToEntityMapper() }
-    single { ElectricStationModelToEntityMapper() }
-    single { UserModelToEntityMapper(get()) }
-
-}
+val appModule = module { }
