9dc161cabb469c79678810617b9f018c8fe4c053
profitsw2000 <profitsw2000@gmail.com>
2022-11-20 22:37:56 +0300

    Добавлена иконка в ресурсы модуля core, частично сделан макет для bottomsheet station info.

--- station_info/src/main/java/chargeit/station_info/ui/StationInfoBottomSheetFragment.kt
+++ station_info/src/main/java/chargeit/station_info/ui/StationInfoBottomSheetFragment.kt
@@ -6,8 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import chargeit.station_info.R
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-class StationInfoBottomSheetFragment : Fragment() {
+class StationInfoBottomSheetFragment : BottomSheetDialogFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,14 +20,5 @@ class StationInfoBottomSheetFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_station_info_bottom_sheet, container, false)
-    }
-
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            StationInfoBottomSheetFragment().apply {
-                arguments = Bundle().apply {
-                }
-            }
     }
 }
