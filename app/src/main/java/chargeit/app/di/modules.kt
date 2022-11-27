3d9c2f313bb004d0c2dc5de1103bf531eb94d082
Ra53n <93147584+Ra53n@users.noreply.github.com>
2022-11-27 21:40:26 +0500

    InfoBottomSheet

--- app/src/main/java/chargeit/app/di/modules.kt
+++ app/src/main/java/chargeit/app/di/modules.kt
@@ -1,12 +1,5 @@
 package chargeit.app.di
 
-import chargeit.app.repository.*
 import org.koin.dsl.module
 
-val appModule = module {
-
-    single<LocalCarRepo> { LocalCarRepoImpl(get()) }
-    single<LocalElectricStationRepo> { LocalElectricStationRepoImpl(get()) }
-    single<LocalSocketRepo> { LocalSocketRepoImpl(get()) }
-    single<LocalUserRepo> { LocalUserRepoImpl(get()) }
-}
+val appModule = module { }
---
+++ station_info/src/main/java/chargeit/station_info/ui/InfoSocketListAdapter.kt
@@ -0,0 +1,52 @@
+package chargeit.station_info.ui
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import chargeit.data.domain.model.Socket
+import chargeit.station_info.R
+import chargeit.station_info.databinding.StationConnectorsListItemBinding
+
+class InfoSocketListAdapter : RecyclerView.Adapter<InfoSocketListAdapter.ViewHolder>() {
+
+    private var data: List<Socket> = arrayListOf()
+    private lateinit var binding: StationConnectorsListItemBinding
+
+    fun setData (data: List<Socket>) {
+        this.data = data
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        binding = StationConnectorsListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding.root)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(data[position])
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return position
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        fun bind(socket: Socket) {
+            with(binding){
+                connectorTypeIconImageView.setImageResource(socket.icon)
+                connectorTypeNameTextView.setText(socket.title)
+            }
+        }
+    }
+
+    private fun getImageRes(id: Int) = when (id) {
+        0 -> chargeit.core.R.drawable.type_1_j1772
+        1 -> chargeit.core.R.drawable.type_2_mannekes
+        else -> chargeit.core.R.drawable.type_1_j1772
+    }
+}
