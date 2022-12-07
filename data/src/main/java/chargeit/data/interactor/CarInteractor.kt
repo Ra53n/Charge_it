ba9bf2a68493e35460848e8ec75a838dc9bcfb79
Ra53n <93147584+Ra53n@users.noreply.github.com>
2022-12-07 22:48:35 +0500

    Перевел проект на локальные базы данных

--- data/src/main/java/chargeit/data/interactor/CarInteractor.kt
+++ data/src/main/java/chargeit/data/interactor/CarInteractor.kt
@@ -1,15 +1,16 @@
 package chargeit.data.interactor
 
 import chargeit.data.repository.LocalCarRepo
+import io.reactivex.rxjava3.core.Flowable
 
 class CarInteractor(
     private val carRepo: LocalCarRepo
 ) {
 
-    fun getAllCarBrands() = carRepo.getAllCar().map { it.brand }
+    fun getAllCarBrands(): Flowable<List<String>> =
+        carRepo.getAllCar().map { list -> list.map { it.brand } }
 
-    fun getAllModelsByBrand(brand: String) =
+    fun getAllModelsByBrand(brand: String): Flowable<List<String>> =
         carRepo.getAllCar()
-            .filter { it.brand == brand }
-            .map { it.model }
+            .map { list -> list.filter { it.brand == brand }.map { it.model } }
 }
--- profileScreen/src/main/java/chargeit/profilescreen/di/modules.kt
+++ profileScreen/src/main/java/chargeit/profilescreen/di/modules.kt
@@ -3,8 +3,8 @@ package chargeit.profilescreen.di
 import chargeit.data.interactor.CarInteractor
 import chargeit.data.interactor.UserInteractor
 import chargeit.data.repository.LocalCarRepo
+import chargeit.data.repository.LocalCarRepoImpl
 import chargeit.profilescreen.data.mapper.UserMapper
-import chargeit.profilescreen.data.repository.FakeCarRepoImpl
 import chargeit.profilescreen.viewmodel.LoginViewModel
 import chargeit.profilescreen.viewmodel.ProfileRegistrationViewModel
 import chargeit.profilescreen.viewmodel.ProfileViewModel
@@ -12,7 +12,7 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val profileScreenModule = module {
-    single<LocalCarRepo> { FakeCarRepoImpl() }
+    single<LocalCarRepo> { LocalCarRepoImpl(get(), get()) }
     factory { CarInteractor(get()) }
     factory { UserMapper() }
     factory { UserInteractor(get()) }
