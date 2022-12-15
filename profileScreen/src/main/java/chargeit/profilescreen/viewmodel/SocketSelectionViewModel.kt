b45ba82082cecf07f0f0c4e28182ae71531f61c7
Ra53n <93147584+Ra53n@users.noreply.github.com>
2022-12-15 22:33:29 +0500

    Добавил возможность поиска сокета и снятия всех отметок в списке сокетов (#35)

--- profileScreen/src/main/java/chargeit/profilescreen/viewmodel/SocketSelectionViewModel.kt
+++ profileScreen/src/main/java/chargeit/profilescreen/viewmodel/SocketSelectionViewModel.kt
@@ -4,12 +4,24 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import chargeit.core.viewmodel.CoreViewModel
 import chargeit.data.domain.model.Socket
+import chargeit.profilescreen.view.adapter.SocketItem
 
 class SocketSelectionViewModel : CoreViewModel() {
-    private val _socketsLiveData = MutableLiveData<List<Socket>>()
-    val socketsLiveData: LiveData<List<Socket>> by this::_socketsLiveData
+    private val _socketsLiveData = MutableLiveData<List<SocketItem>>()
+    val socketsLiveData: LiveData<List<SocketItem>> by this::_socketsLiveData
+
+    private val _filteredSocketsLiveData = MutableLiveData<List<SocketItem>>()
+    val filteredSocketsLiveData: LiveData<List<SocketItem>> by this::_filteredSocketsLiveData
+
+    private val socketList = Socket.getAllSockets().map { SocketItem(it, false) }
+
 
     fun requireSocketsList() {
-        _socketsLiveData.value = Socket.getAllSockets()
+        _socketsLiveData.value = Socket.getAllSockets().map { SocketItem(it, false) }
+    }
+
+    fun filterSockets(text: String) {
+        _filteredSocketsLiveData.value =
+            socketList.filter { it.socket.title.contains(text, ignoreCase = true) }
     }
 }
