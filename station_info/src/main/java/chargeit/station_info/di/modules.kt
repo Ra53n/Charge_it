dbf7e62685f85385030cda265089dc6e1686db27
profitsw2000 <profitsw2000@gmail.com>
2022-12-15 21:17:25 +0300

    Исправлен баг, из-за которого при загрузке на карте не отображались маркеры со станциями зарядки. В модуле stationInfoModule для di при создании объекту класса LocalElectricStationRepo присваивается имя.

--- station_info/src/main/java/chargeit/station_info/di/modules.kt
+++ station_info/src/main/java/chargeit/station_info/di/modules.kt
@@ -7,13 +7,14 @@ import chargeit.data.room.mappers.ElectricStationModelToEntityMapper
 import chargeit.data.room.mappers.SocketModelToEntityMapper
 import chargeit.station_info.presentation.viewmodel.FullStationInfoViewModel
 import chargeit.station_info.presentation.viewmodel.StationInfoBottomSheetViewModel
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val stationInfoModule = module {
     factory { SocketModelToEntityMapper() }
     factory { ElectricStationModelToEntityMapper(get()) }
-    single<LocalElectricStationRepo> { LocalElectricStationRepoImpl(get(),get()) }
-    factory { ElectricStationInteractor(get()) }
+    single<LocalElectricStationRepo>(named("localElectricStationRepo")) { LocalElectricStationRepoImpl(get(),get()) }
+    factory { ElectricStationInteractor(get(named("localElectricStationRepo"))) }
     single { StationInfoBottomSheetViewModel(get(),get()) }
     single { FullStationInfoViewModel(get()) }
 }
